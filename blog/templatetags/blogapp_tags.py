f4b844bee20cbfa6fa6b737845f5e6409a3c89b8
Paweł Grajewski <unionproject88@gmail.com>
2022-01-26 19:05:20 +0000

    add template tags

---
+++ blog/templatetags/blogapp_tags.py
@@ -0,0 +1,13 @@
+from blog.models import BlogPage
+from django.template import Library, loader
+
+register = Library()
+
+@register.inclusion_tag('blog/components/posts_list.html',
+takes_context=True)
+def posts_list(context):
+  posts = BlogPage.objects.all()
+  return {
+  'request': context['request'],
+  'posts': posts
+  }
